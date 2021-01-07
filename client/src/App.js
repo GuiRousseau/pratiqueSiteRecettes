3242a844643873a61df5f7eae838fd0c4858db95
Guillaume Rousseau <gui-gui999@live.ca>
2021-01-07 15:33:13 -0500

    tuto complete

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import List from './pages/List';
+import './App.css';
+
+class App extends Component {
+  render() {
+    const App = () => (
+      <div>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/list' component={List}/>
+        </Switch>
+      </div>
+    )
+    return (
+      <Switch>
+        <App/>
+      </Switch>
+    );
+  }
+}
+
+export default App;
