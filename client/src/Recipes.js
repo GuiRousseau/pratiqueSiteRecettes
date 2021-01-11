f7cdf2af1af588187fad6022e8e4e513ae7fada7
Guillaume Rousseau <gui-gui999@live.ca>
2021-01-11 13:48:55 -0500

    changement nom de DB et changement en js

--- client/src/Recipes.js
+++ client/src/Recipes.js
@@ -1,95 +1,94 @@
-{
+export const recettesData = {
     "recipes" : {
         "Old Fashioned" : {
             "ingredients" : ["2 oz. Whiskey", "4-5 goutes Angostura bitters", "Un peu de sirop/sucre"],
+            "desc" : "petite description",
             "steps" : ["Brasser dans un verre old-fashionned avec une grosse glace", "Ajouter des cerises de marasquin et une pelure d’orange"],
             "tags" : ["angostura bitters", "angostura", "bitters", "bourbon", "whiskey"]
         },
         "Manhatan" : {
             "ingredients" : ["2 oz. Whiskey", "¾ oz. Vermouth rouge", "4-5 goutes Angostura bitters"],
+            "desc" : "petite description",
             "steps" : ["Brasser dans un verre old-fashionned avec une grosse glace", "Ajouter des cerises de marasquin"],
             "tags" : ["angostura bitters", "angostura", "bitters", "vermouth rouge", "vermouth", "rouge", "bourbon", "whiskey"]
         },
         "Mojito" : {
             "ingredients" : ["2 oz. Rhum", "4-6 feuilles Menthe", "½ oz. Sirop", "¾ oz. Lime"],
+            "desc" : "petite description",
             "steps" : ["Écraser la menthe légèrement et tout mélanger dans le shaker", "Double tamiser dans un highball rempli de glace", "Remplir reste Soda"],
             "tags" : ["menthe", "rhum"]
         },
         "Old Cuban" : {
             "ingredients" : ["1 ½ oz. Rhum", "4-6 feuilles Menthe", "1 oz. Sirop", "¾ oz. Lime", "2 goutes Angostura bitters", "2 oz. Bulles"],
+            "desc" : "petite description",
             "steps" : ["Dans un shaker, mélanger menthe, sirop, rhum, lime, bitters et glace", "Verser avec double tamis dans une coupe", "Ajouter bulles et une feuille menthe pour garnir"],
             "tags" : ["angostura bitters", "angostura", "bitters", "champagne", "menthe", "mousseux", "rhum"]
         },
         "Devil's Eye" : {
             "ingredients" : ["1 ½ oz. Gin", "½ oz. Curaçao bleu", "½ oz. Citron"],
+            "desc" : "petite description",
             "steps" : ["Shaker avec glace et verser dans un verre à martini"],
-            "tags" : ["TODO : completer"]
+            "tags" : ["bleu", "curacao", "curaçao", "gin"]
         },
         "Blue Yeti" : {
             "ingredients" : ["2 ½ oz. Curaçao bleu", "1 ½ oz. Gin", "1 oz. Lime", "4 oz. Soda tonique"],
-            "steps" : ["Shaker et verser dans un verre avec glaces", "allonger avec le tonique"]
+            "desc" : "petite description",
+            "steps" : ["Shaker et verser dans un verre avec glaces", "allonger avec le tonique"],
+            "tags" : ["bleu", "curacao", "curaçao", "gin"]
         },
         "White Lady" : {
             "ingredients" : ["1 oz. Gin", "1 oz. Triple Sec", "1 oz. Citron"],
-            "steps" : ["Shaker avec glace et verser dans un verre à martini"]
+            "desc" : "petite description",
+            "steps" : ["Shaker avec glace et verser dans un verre à martini"],
+            "tags" :["gin", "Triple Sec"]
         },
         "Cocktail à l’Orange" : {
             "ingredients" : ["2 oz. Gin", "2 oz. Triple Sec", "3 ⅓ Jus d’orange", "1 goutte Citron"],
-            "steps" : ["Shaker avecc glace et verser dans un verre à martini"]
+            "desc" : "petite description",
+            "steps" : ["Shaker avecc glace et verser dans un verre à martini"],
+            "tags" : ["gin", "orange", "d'orange", "Triple Sec"]
         },
         "L'Ellipse" : {
             "ingredients" : ["1 ¾ oz. Gin", "1 oz. Jus d’ananas", "1 oz. Purée d’ananas", "½ oz. Lime", "0,4 oz. Sirop ", "5 traits mix Paprika et Cayenne", "6 gouttes Solution saline"],
-            "steps" : ["Shaker et double tamiser dans verre old-fashionned", "Saupoudrer mix ou tranche de citron mince"]
+            "desc" : "petite description",
+            "steps" : ["Shaker et double tamiser dans verre old-fashionned", "Saupoudrer mix ou tranche de citron mince"],
+            "tags" : ["ananas", "ellipse", "gin"]
         },
         "Le Classique" : {
             "ingredients" : ["2 oz. Gin", "Environ ½ fruit frais", "½ oz. Sirop", "½ Lime", "Soda"],
-            "steps" : ["Shaker et double tamiser dans verre rempli de glace", "Remplir Soda "]
+            "desc" : "petite description",
+            "steps" : ["Shaker et double tamiser dans verre rempli de glace", "Remplir Soda "],
+            "tags" : ["classique", "fruit", "gin"]
         },
         "Le Classique (grenadine)" : {
             "ingredients" : ["2 oz. Gin", "½ oz. Citron ", "½ oz. Sirop", "½ Lime", "Petit peu Grenadine", "Soda"],
-            "steps" : ["Shaker et verser dans verre rempli de glace", "Remplir Soda "]
+            "desc" : "petite description",
+            "steps" : ["Shaker et verser dans verre rempli de glace", "Remplir Soda "],
+            "tags" : ["classique", "gin", "grenadine"]
         },
         "MISS X" : {
             "ingredients" : ["1 oz. Gin", "1/2 oz. Citron", "⅓ oz. Sirop", "1 oz. Jus de melon d’eau", "2 oz. Bulles"],
-            "steps" : ["Shaker et verser dans une coupe à martini ou fancy large", "Pelure de citron ou tranche de citron déshydratée comme déco"]
+            "desc" : "petite description",
+            "steps" : ["Shaker et verser dans une coupe à martini ou fancy large", "Pelure de citron ou tranche de citron déshydratée comme déco"],
+            "tags" : ["champagne", "gin", "melon", "mousseux"]
         },
         "Pool Party" : {
             "ingredients" : ["1 ½ oz. Gin", "½ oz. Lime", "½ oz. Sirop", "1 oz. Jus de melon d’eau", "5 feuilles Basilique", "5 feuilles Menthe", "1 oz. Soda"],
-            "steps" : ["Remuer à la cuillère dans un highball rempli de glaces"]
+            "desc" : "petite description",
+            "steps" : ["Remuer à la cuillère dans un highball rempli de glaces"],
+            "tags" : ["basilique", "gin", "menthe", "melon"]
         },
         "Tequila Sunrise" : {
             "ingredients" : ["2 oz. Tequila", "4 oz. Jus d’orange", "grenadine"],
-            "steps" : ["Shaker et verser dans un verre ", "Ajouter Grenadine jusqu’à voir un coucher de soleil"]
+            "desc" : "petite description",
+            "steps" : ["Shaker et verser dans un verre ", "Ajouter Grenadine jusqu’à voir un coucher de soleil"],
+            "tags" : ["grenadine", "orange", "d'orange", "sunrise", "tequila"]
         },
         "Paloma" : {
             "ingredients" : ["1 ½ oz. Tequila", "3 oz. Jus de pamplemousse rose", "2 oz. Soda"],
-            "steps" : ["Tout mélanger dans un verre old-fashionned avec de la glace", "Décorer de zeste de pamplemousse"]
+            "desc" : "petite description",
+            "steps" : ["Tout mélanger dans un verre old-fashionned avec de la glace", "Décorer de zeste de pamplemousse"],
+            "tags" : ["pamplemousse", "rose", "tequila"]
         }
-    },
-    "tags" : {
-        "ananas" : ["Ellipse"],
-        "angostura bitters" : ["Manhatan", "Old Fashioned", "Old Cuban"],
-        "basilique" : ["Pool Party"],
-        "bleu" : ["Devil's Eye", "Blue Yeti"],
-        "bourbon" : ["Manhatan", "Old Fashioned"],
-        "champagne" : ["Old Cuban", "MISS X"],
-        "classique" : ["Le Classique", "Le Classique (grenadine)"],
-        "curacao" : ["Devil's Eye", "Blue Yeti"],
-        "curaçao" : ["Devil's Eye", "Blue Yeti"],
-        "elipse" : ["L'Ellipse"],
-        "fruit" : ["Le Classique"],
-        "gin" : ["Devil's Eye", "Blue Yeti", "White Lady", "Cocktail à l’Orange", "L’Ellipse", "Le Classique", "Le Classique (grenadine)", "MISS X", "Pool Party"],
-        "grenadine" : ["Le Classique (grenadine)", "Tequila Sunrise"],
-        "menthe" : ["Mojito", "Old Cuban", "Pool Party"],
-        "melon" : ["MISS X", "Pool Party"],
-        "mousseux" : ["Old Cuban", "MISS X"],
-        "orange" : ["Cocktail à l’Orange", "Tequila Sunrise"],
-        "d'orange" : ["Cocktail à l’Orange", "Tequila Sunrise"],
-        "rhum" : ["Mojito", "Old Cuban"],
-        "sunrise" : ["Tequila Sunrise"],
-        "tequila" : ["Tequila Sunrise", "Paloma"],
-        "Triple Sec" : ["White Lady", "Cocktail à l’Orange"],
-        "vermouth rouge" : ["Manhatan"],
-        "whiskey" : ["Manhatan", "Old Fashioned"]
     }
 }
