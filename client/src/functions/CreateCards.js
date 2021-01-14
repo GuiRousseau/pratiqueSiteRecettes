2c878555289b6d7e3d3b2b7a43ec1e9883da072f
Guillaume Rousseau <gui-gui999@live.ca>
2021-01-14 14:23:30 -0500

    ajout de ";"

--- client/src/functions/CreateCards.js
+++ client/src/functions/CreateCards.js
@@ -6,7 +6,7 @@ export default function CreateCards(names, recipes){
     while(names.length) newArr.push(names.splice(0,3));
     
     var rows = newArr.map( (entry)=>{
-      const cardsOfRow = GenerateCards(entry, recipes)
+      const cardsOfRow = GenerateCards(entry, recipes);
       return(
       <Grid container item key={Math.random()} xs={12} spacing={3} >
         {cardsOfRow}
--- client/src/functions/FiltrerRecettes.js
+++ client/src/functions/FiltrerRecettes.js
@@ -5,7 +5,7 @@ export default function FiltrerRecettes(searchTags, tagsRecettes, recipes){
     var names = new Set([]);
     searchTags.forEach(function(entry) {
       Object.keys(tagsRecettes).forEach(function(entry2) {
-        var tab = tagsRecettes[entry2]
+        var tab = tagsRecettes[entry2];
         if(tab.includes(entry.toLowerCase()) || String(entry2.toLowerCase()).includes(String(entry.toLowerCase()))){
           names.add(entry2);
         }
